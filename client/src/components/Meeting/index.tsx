51c76d08d8b4caf486fccc6ac5d070a41fb5edce
Akshat Garg <akshatarungarg78@gmail.com>
2021-06-16 20:45:38 +0530

    replaced peerjs with simple-peer on client

--- client/src/components/Meeting/index.tsx
+++ client/src/components/Meeting/index.tsx
@@ -2,7 +2,7 @@
 import { useRef, useState } from "react";
 import { useEffect } from "react";
 import socket from '../../config/socket'
-import Peer from 'peerjs'
+import Peer from "simple-peer";
 export interface MeetingProps {
     
 }
@@ -10,78 +10,102 @@ const Video = (props:any) => {
 
     const videoRef = useRef<HTMLVideoElement>(null)
     useEffect(()=>{
-        if(videoRef.current)
-            videoRef.current.srcObject = props.stream
+        props.peer.on("stream", (stream:any) => {
+            if(videoRef.current)
+                videoRef.current.srcObject = stream
+        })
     }, [])
     return (
         <video ref={videoRef} autoPlay />
     )
 
 }
+
+const videoConstraints = {
+height: window.innerHeight / 2,
+width: window.innerWidth / 2
+};
+
 const Meeting = (): JSX.Element => {
-    const myVideo = useRef<HTMLVideoElement>(null)
-    const [streams, setStreams] = useState<any>([])
-    const peers:any = {}
-    const ROOM_ID = '123456'
-    const myPeer = new Peer()
-    useEffect(()=>{
-        // connect to the room
-        socket.connect() 
-        myPeer.on('open', id => {
-            console.log(id)
-            socket.emit('join-room', ROOM_ID, id)
-        })
-        // take the audio and video of myself
-        navigator.mediaDevices.getUserMedia({
-            audio : false,
-            video : true
-        }).then(stream => {
-            if(myVideo.current)
-                myVideo.current.srcObject = stream
-            
-             // get the lists of users already in the meeting and connect to them 
-             // from the event 'all-users'
-            socket.on('all-users', (userIds) => {
-                console.log("userIds: ", userIds)
-                userIds.forEach((id:any) => connectToNewUser(id, stream))
+    const [peers, setPeers] = useState<Array<any>>([]);
+    const socketRef = useRef(socket);
+    const userVideo = useRef<HTMLVideoElement>(null);
+    const peersRef = useRef<Array<any>>([]);
+    const roomID ='123456';
+
+    useEffect(() => {
+        socket.connect()
+        navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
+            if(userVideo.current)
+                userVideo.current.srcObject = stream;
+
+            socketRef.current.emit("join room", roomID);
+            socketRef.current.on("all users", users => {
+                const peers:any = [];
+                users.forEach((userID:string) => {
+                    const peer = createPeer(userID, socketRef.current.id, stream);
+                    peersRef.current.push({
+                        peerID: userID,
+                        peer,
+                    })
+                    peers.push(peer);
+                })
+                setPeers(peers);
             })
 
-            // accept incoming calls
-            myPeer.on('call', call => {
-              call.answer(stream)
-              call.on('stream', userVideoStream => {
-                setStreams((prev:any) => [...prev, userVideoStream])
-              })
-            })
-          
-            // connect to new users
-            socket.on('user-connected', userId => {
-              console.log('new user connected')
-              connectToNewUser(userId, stream)
-            })
+            socketRef.current.on("user joined", payload => {
+                const peer = addPeer(payload.signal, payload.callerID, stream);
+                peersRef.current.push({
+                    peerID: payload.callerID,
+                    peer,
+                })
+
+                setPeers(users => [...users, peer]);
+            });
+
+            socketRef.current.on("receiving returned signal", payload => {
+                const item = peersRef.current.find(p => p.peerID === payload.id);
+                item.peer.signal(payload.signal);
+            });
         })
-        // if a user leaves destroy that p2p connection
-        function connectToNewUser(userId:any, stream:any) {
-            const call = myPeer.call(userId, stream)
-            // const video = document.createElement('video')
-            call.on('stream', userVideoStream => {
-                setStreams((prev:any) => [...prev, userVideoStream])
-            //   addVideoStream(video, userVideoStream)
-            })
-            call.on('close', () => {
-                console.log('remove disconnected user video')
-                //   video.remove()
-            })
-            peers[userId] = call
-        } 
-    }, [])
-    console.log(peers)
+    }, []);
+
+    function createPeer(userToSignal:any, callerID:any, stream:any) {
+        const peer = new Peer({
+            initiator: true,
+            trickle: false,
+            stream,
+        });
+
+        peer.on("signal", signal => {
+            socketRef.current.emit("sending signal", { userToSignal, callerID, signal })
+        })
+
+        return peer;
+    }
+
+    function addPeer(incomingSignal:any, callerID:any, stream:any) {
+        const peer = new Peer({
+            initiator: false,
+            trickle: false,
+            stream,
+        })
+
+        peer.on("signal", signal => {
+            socketRef.current.emit("returning signal", { signal, callerID })
+        })
+
+        peer.signal(incomingSignal);
+
+        return peer;
+    }
+
     return (
         <div>
-            <video autoPlay ref = {myVideo} style={{width:'100px' ,height:'100px', objectFit:'cover', zIndex:1000}} />
+            <video autoPlay ref = {userVideo} style={{width:'100px' ,height:'100px', objectFit:'cover', zIndex:1000}} />
             {
-                streams.map((stream:any) => {
-                    return <Video  stream = {stream} />
+                peers.map((peer:any, index : any) => {
+                    return <Video  key = {index} peer = {peer} />
                 })
             }
         </div>
--- server/src/index.ts
+++ server/src/index.ts
@@ -32,28 +32,47 @@ io.attach(server, {
 });
 
 
-let usersOfRoom:any = {}
-io.on('connection', (socket) => {
-  socket.on('join-room', (roomId, userId) => {
-    console.log(usersOfRoom)
-    // save the info in a database.
-    if(usersOfRoom[roomId]){
-      usersOfRoom[roomId].push(userId)
-    }else{
-      usersOfRoom[roomId] = [userId]
-    }
-    // room id and active users
-    socket.emit('all-users', usersOfRoom[roomId])
-    // whenever this event is emitted , send user details to connect at client
-    socket.emit('user-connected', userId)
-    // send a all-users list for the person connected so that they can connect with everyone else
-    // if disconnected send the signal to destroy this peer connection
+const users:any = {};
+
+const socketToRoom:any = {};
+
+io.on('connection', socket => {
+    socket.on("join room", roomID => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter((id:any) => id !== socket.id);
+
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
     socket.on('disconnect', () => {
-      socket.broadcast.to(roomId).emit('user-disconnected', userId)
-    })
-  
-  })
-})
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter((id:any) => id !== socket.id);
+            users[roomID] = room;
+        }
+    });
+
+});
+
 
 const serverStart = async () => {
   try{
