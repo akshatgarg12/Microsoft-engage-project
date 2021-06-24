345978ecc8c888c76e7bdf8dccf61ce2da979a9d
Akshat Garg <akshatarungarg78@gmail.com>
2021-06-24 21:50:43 +0530

    added audio on off and video on off feature

--- client/src/components/Meeting/index.tsx
+++ client/src/components/Meeting/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useRef, useState, useEffect, useReducer } from "react";
 import {Prompt, useHistory} from 'react-router-dom'
-import {Flex} from '@fluentui/react-northstar'
+import {Button, Flex} from '@fluentui/react-northstar'
 import socket from '../../config/socket'
 import Peer from "simple-peer";
 import classes from './style.module.css'
@@ -44,6 +44,10 @@ const Video = (props:any) => {
             console.log(e)
             setPeerError(true)
         })
+        props.peer.on('close', () => {
+            setPeerError(true)
+        })
+        if(props.peer.destroyed) setPeerError(true)
     }, [])
     if(peerError) return null
     return (
@@ -54,12 +58,6 @@ const Video = (props:any) => {
     )
 
 }
-
-const videoConstraints = {
-    height: window.innerHeight / 2,
-    width: window.innerWidth / 2
-};
-
 const Meeting = ({meetingId}:MeetingProps): JSX.Element => {
     /* 
         1. first check whether the meeting exists and is active.
@@ -70,118 +68,172 @@ const Meeting = ({meetingId}:MeetingProps): JSX.Element => {
     const [peers, setPeers] = useState<Array<any>>([]);
     const socketRef = useRef(socket);
     const userVideo = useRef<HTMLVideoElement>(null);
+    const [userLeft, setUserLeft] = useState(false)
     const peersRef = useRef<Array<any>>([]);
     const history = useHistory()
-
-    // const getUserStream = async () => {
-    //     const stream = await navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: false })
-    //     return stream
-    // }
-    // const stopUserStream = (stream:any) => {
-    //     console.log(stream)
-    //     stream.getTracks().forEach((track:any) => {
-    //         console.log(track)
-    //         track.stop()
-    //     })
-    // }
-
+    const [streamOptions, setStreamOptions] = useState({
+        video : true,
+        audio : false
+    })
+    const [userStream, setUserStream] = useState<MediaStream | null>(null)
+    
+    // stops to reload if done by mistake
+    function confirmExit(){
+        // eslint-disable-next-line no-restricted-globals
+        const exit = confirm("Are you sure you want to reload?")
+        if(exit){
+            // leave meeting
+            history.push('/')
+        }else{
+            console.log('unload event unmount')
+        }
+        return "";
+    }
+    function DestroyConnections(){
+        socketRef.current.emit('leaving-meeting')
+    }
+    function LeaveMeeting(){
+        try{
+            peers.forEach(({peer}) => {
+                peer.destroy()
+            })
+        }catch(e){
+            console.log(e)
+        }finally{
+            setUserLeft(true)
+            CloseMedia()
+            history.push('/')
+        }
+    }
+    function CloseMedia(){
+        userStream && userStream.getTracks().forEach((track) => {
+            console.log(track)
+            track.stop()
+        })
+        setStreamOptions({video:false, audio:false})
+        setUserStream(null)
+    }
     useEffect(() => {
-       // connect socket
-       socketRef.current.connect()
-       // get user media
-       navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: false }).then(stream => {
-           if(userVideo.current)
-               userVideo.current.srcObject = stream;
-           // emit signal to join room
-           socketRef.current.emit("join-meeting", meetingId);
-           // if auth error
-           socketRef.current.on("user-auth-fail", (payload) => {
-               // push user back to dashboard
-               socketRef.current.disconnect()
-               history.push('/')
-           })
-           // get all users in the room
-           socketRef.current.on("meeting-members", (payload : any) => {
-               const {members, from} = payload
-               console.log(payload)
-               const peers:any = [];
-               members && members.forEach((to:any) => {
-                   const {socketId} = to;
-                   // create peer of all members by sending signal 
-                   const peer = sendOffer(to, from, stream);
-                   peersRef.current.push({
-                       peerID: socketId,
-                       peer,
-                   })
-                   peers.push({peer, info : to});
-               })
-               setPeers(peers);
-           })
-           // when new user joins, wait for their signal and then accept and return an answer
-           socketRef.current.on("user-offer", payload => {
-               const {signal, from, to} = payload
-               const peer = sendAnswer(signal, from, to, stream);
-               // remove the one with socketId which has been destroyed
-               peersRef.current.push({
-                   peerID: from.socketId,
-                   peer,
-               })
-               // const updatedPeers = [...peers].filter((p) =>{ 
-               //     if(p.info.userId === from.userId) p.peer.destroy()
-               //     return  p.info.userId !== from.userId
-               // })
-               // console.log(updatedPeers)
-               // updatedPeers.push({peer, info : from})
-               // console.log(updatedPeers)
-               // setPeers(updatedPeers)
-               setPeers((prev) => ([...prev, {peer, info:from}]))
-           });
+        const {audio, video} = streamOptions
+        if(!audio && !video){
+            if(userVideo.current)
+               userVideo.current.srcObject = null;
+            try{
+                peers.forEach(({peer}) => {
+                    if(userStream && !peer.destroyed){
+                        peer.removeStream()
+                        peer.addStream(undefined)
+                    }
+                })
+                setUserStream(null)
+            }catch(e){
+                // console.error(e)
+            }
+        }else{
+            navigator.mediaDevices.getUserMedia(streamOptions).then((stream) => {
+                // get this new stream and customise the stream to all peers
+                if(userVideo.current)
+                   userVideo.current.srcObject = stream;
+                try{
+                    peers.forEach(({peer}) => {
+                        if(userStream && !peer.destroyed){
+                            peer.removeStream()
+                            peer.addStream(stream)
+                        }
+                    })
+                    setUserStream(stream)
+                }catch(e){
+                    // console.error(e)
+                }
+                
+            })
+        }
+        
+    }, [streamOptions])
+   
+    useEffect(() => {
+        try{
+            // connect socket
+            socketRef.current.connect()
+            // get user media
+            navigator.mediaDevices.getUserMedia(streamOptions).then(stream => {
+                setUserStream(stream)
+                if(userVideo.current)
+                    userVideo.current.srcObject = stream;
+                // emit signal to join room
+                socketRef.current.emit("join-meeting", meetingId);
+                // if auth error
+                socketRef.current.on("user-auth-fail", (payload) => {
+                    // push user back to dashboard
+                    socketRef.current.disconnect()
+                    history.push('/')
+                })
+                // get all users in the room
+                socketRef.current.on("meeting-members", (payload : any) => {
+                    const {members, from} = payload
+                    console.log(payload)
+                    const peers:any = [];
+                    members && members.forEach((to:any) => {
+                        const {socketId} = to;
+                        // create peer of all members by sending signal 
+                        const peer = sendOffer(to, from, stream);
+                        peersRef.current.push({
+                            peerID: socketId,
+                            peer,
+                        })
+                        peers.push({peer, info : to});
+                    })
+                    setPeers(peers);
+                })
+                // when new user joins, wait for their signal and then accept and return an answer
+                socketRef.current.on("user-offer", payload => {
+                    const {signal, from, to} = payload
+                    const peer = sendAnswer(signal, from, to, stream);
+                    // remove the one with socketId which has been destroyed
+                    peersRef.current.push({
+                        peerID: from.socketId,
+                        peer,
+                    })
+                   
+                    setPeers((prev) => ([...prev, {peer, info:from}]))
+                });
 
-           socketRef.current.on("receive-answer", payload => {
-               const {signal, from} = payload
-               // check who sent the signal and accept it.(completes the handshake and establishes conn)
-               const item = peersRef.current.find(p => p.peerID === from.socketId);
-               item.peer.signal(signal);
-           });
-           socketRef.current.on("leave-meeting" , (payload) => {
-               const socketId = socketRef.current.id
-               peersRef.current.forEach((peer) => {
-                   socketRef.current.emit("leaving", {from:socketId,to:peer.peerID})
-               })
-           })
-           socketRef.current.on("leaving" , payload => {
-               const {from} = payload
-               peersRef.current = peersRef.current.filter((p) => {
-                  if(p.peerID === from) p.peer.destroy()
-                  return p.peerID !== from
-               })
-               // remove from peer array aswell
-               const newPeerArray = [...peers].filter((p) => {
-                   if(p.info.socketId === from) p.peer.destroy()
-                   return p.info.socketId !== from
-               })
-               setPeers(newPeerArray)
-           })
-       })
-       // stops to reload if done by mistake
-       function confirmExit(){
-           // eslint-disable-next-line no-restricted-globals
-           const exit = confirm("Are you sure you want to reload?")
-           if(exit){
-               // leave meeting
-               history.push('/')
-           }else{
-               console.log('unload event unmount')
-           }
-           return "";
-       }
-       function DestroyConnections(){
-           socketRef.current.emit('leaving-meeting')
-       }
-       window.onbeforeunload = confirmExit;
-       window.onunload = DestroyConnections;
-       window.onclose = DestroyConnections;
-       console.log(peers)
+                socketRef.current.on("receive-answer", payload => {
+                    const {signal, from} = payload
+                    // check who sent the signal and accept it.(completes the handshake and establishes conn)
+                    const item = peersRef.current.find(p => p.peerID === from.socketId);
+                    // if(item.peer.destroyed)
+                        item.peer.signal(signal);
+                });
+                socketRef.current.on("leave-meeting" , (payload) => {
+                    const socketId = socketRef.current.id
+                    peersRef.current.forEach((peer) => {
+                        socketRef.current.emit("leaving", {from:socketId,to:peer.peerID})
+                    })
+                })
+                socketRef.current.on("leaving" , payload => {
+                    const {from} = payload
+                    peersRef.current = peersRef.current.filter((p) => {
+                        if(p.peerID === from) p.peer.destroy()
+                        return p.peerID !== from
+                    })
+                    // remove from peer array aswell
+                    const newPeerArray = [...peers].filter((p) => {
+                        if(p.info.socketId === from) p.peer.destroy()
+                        return p.info.socketId !== from
+                    })
+                    setPeers(newPeerArray)
+                })
+            })
+            
+            window.onbeforeunload = confirmExit;
+            window.onunload = DestroyConnections;
+            window.onclose = DestroyConnections;
+            console.log(peers)
+        }catch(e){
+            // console.log(e)
+            history.push('/')
+        }
     }, []);
 
     function sendOffer(to:any, from:any, stream:any) {
@@ -194,7 +246,6 @@ const Meeting = ({meetingId}:MeetingProps): JSX.Element => {
         peer.on("signal", signal => {
             socketRef.current.emit("sending-signal", { to, from, signal})
         })
-
         return peer;
     }
 
@@ -212,18 +263,21 @@ const Meeting = ({meetingId}:MeetingProps): JSX.Element => {
         })       
         return peer;
     }
-    console.log(peers)
+    // console.log(peers)
     return (
         <>
             <Flex wrap className={classes.container}>
-                <video className={classes.styledVideo} autoPlay ref = {userVideo} />
+                <Button content="leave" onClick={LeaveMeeting} />
+                <Button content={streamOptions.audio ? 'Audio Off' : 'Audio On'} onClick={() => setStreamOptions((prev) => ({...prev , audio:!streamOptions.audio}))}/>
+                <Button content={streamOptions.video ? 'Video Off' : 'Video On'} onClick={() => setStreamOptions((prev) => ({...prev , video:!streamOptions.video}))}/>
+                <video className={classes.styledVideo} autoPlay ref={userVideo} />
                 {
                     peers.map((value:any, index : any) => <Video key = {index} peer = {value.peer} info = {value.info} />)
                 }
                 
             </Flex>
             <Prompt
-                when={true}
+                when={!userLeft}
                 message="Are you sure you want to leave?" />
         </>
 
