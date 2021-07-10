5802264397e1edfbe3e39c32eb300f5ba0ac57bf
Akshat Garg <akshatarungarg78@gmail.com>
2021-07-11 00:34:41 +0530

    fetch meeting chats and server side implementation

--- server/src/routes/chat.ts
+++ server/src/routes/chat.ts
@@ -1,8 +1,8 @@
 import {Router} from 'express'
-import { getChats } from '../controllers/chat'
+import { getChats, getMeetingChats } from '../controllers/chat'
 
 const router = Router()
 
 router.post('/chat', getChats)
-
+router.post('/meeting/chat', getMeetingChats)
 export default router
